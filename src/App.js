631beed44a55a4e6fa65b038baeef742851d40f7
DragosBasca <dalphaiqv2@gmail.com>
2024-09-04 18:03:32 +0300

    Rethought router process. Made manual.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './assets/css/App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
+import Grammar from "./pages/Grammar";
+import Dictionary from "./pages/Dictionary";
+import Counters from "./pages/Counters";
+import KanjiPage from "./pages/KanjiPage";
 
 function App() {
   return (
-      <Home />
+      <BrowserRouter>
+          <Routes>
+              <Route path={"/"} exact element={<Home />}>
+                  <Route index element={<Grammar />}/>
+                  <Route path={"grammar"} element={<Grammar />}/>
+                  <Route path={"dictionary"} element={<Dictionary/>}/>
+                  <Route path={"counters"} element={<Counters/>}/>
+                  <Route path={"kanji"} element={<KanjiPage />}/>
+              </Route>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,48 @@
+import {Link, Outlet} from "react-router-dom";
 import styles from "../assets/css/pages/Home.module.css";
 import {useState} from "react";
-import Grammar from "./Grammar";
-import Dictionary from "./Dictionary";
-import Counters from "./Counters";
-import KanjiPage from "./KanjiPage";
 
 const Home = () => {
-    const [selectedPage, setSelectedPage] = useState("grammar");
+    const [selected, setSelected] = useState(0);
 
     return (
         <div className={styles.main}>
             <header>
-                <button onClick={() => {
-                    setSelectedPage("grammar")
-                }}
-                        className={selectedPage === "grammar" ? styles.selected : ""}
-                >Grammar</button>
-                <button onClick={() => {
-                    setSelectedPage("dictionary")
-                }}
-                        className={selectedPage === "dictionary" ? styles.selected : ""}
-                >Dictionary</button>
-                <button onClick={() => {
-                    setSelectedPage("counters")
-                }}
-                        className={selectedPage === "counters" ? styles.selected : ""}
-                >Counters</button>
-                <button onClick={() => {
-                    setSelectedPage("kanji")
-                }}
-                        className={selectedPage === "kanji" ? styles.selected : ""}
-                >Kanji</button>
+                <button>
+
+                </button>
+                <Link to={"grammar"}
+                      className={selected === 0 ? styles.selected : ""}
+                      onClick={() => {setSelected(0)}}
+                >
+                    Grammar
+                </Link>
+                <Link to={"dictionary"}
+                      className={selected === 1 ? styles.selected : ""}
+                      onClick={() => {
+                    setSelected(1)}}
+                >
+                    Dictionary
+                </Link>
+                <Link to={"counters"}
+                      className={selected === 2 ? styles.selected : ""}
+                      onClick={() => {
+                          setSelected(2)}}
+                >
+                    Counters
+                </Link>
+                <Link to={"kanji"}
+                      className={selected === 3 ? styles.selected : ""}
+                      onClick={() => {
+                          setSelected(3)}}
+                >
+                    Kanji
+                </Link>
             </header>
             <section>
                 <div className={styles.wrapper1}>
                     <div className={styles.wrapper2}>
-                        {selectedPage === "grammar" && (<Grammar />)}
-                        {selectedPage === "dictionary" && (<Dictionary />)}
-                        {selectedPage === "counters" && (<Counters />)}
-                        {selectedPage === "kanji" && (<KanjiPage />)}
+                        <Outlet/>
                     </div>
                 </div>
             </section>
