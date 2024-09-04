2cd178c4e355445fde386560c8558570685a54b3
DragosBasca <dalphaiqv2@gmail.com>
2024-09-04 18:13:36 +0300

    Rethought router process. Made manual.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,32 @@
-import {Link, Outlet} from "react-router-dom";
 import styles from "../assets/css/pages/Home.module.css";
 import {useState} from "react";
+import Grammar from "./Grammar";
+import Dictionary from "./Dictionary";
+import Counters from "./Counters";
+import KanjiPage from "./KanjiPage";
 
 const Home = () => {
-    const [selected, setSelected] = useState(0);
+    const [selected, setSelected] = useState("grammar");
+    const pages = ["grammar", "dictionary", "counters", "kanji"];
 
     return (
         <div className={styles.main}>
             <header>
-                <button>
-
-                </button>
-                <Link to={"grammar"}
-                      className={selected === 0 ? styles.selected : ""}
-                      onClick={() => {setSelected(0)}}
-                >
-                    Grammar
-                </Link>
-                <Link to={"dictionary"}
-                      className={selected === 1 ? styles.selected : ""}
-                      onClick={() => {
-                    setSelected(1)}}
-                >
-                    Dictionary
-                </Link>
-                <Link to={"counters"}
-                      className={selected === 2 ? styles.selected : ""}
-                      onClick={() => {
-                          setSelected(2)}}
-                >
-                    Counters
-                </Link>
-                <Link to={"kanji"}
-                      className={selected === 3 ? styles.selected : ""}
-                      onClick={() => {
-                          setSelected(3)}}
-                >
-                    Kanji
-                </Link>
+                {pages.map((page, index)=>(
+                    <button onClick={()=>{setSelected(page)}}
+                            className={selected === page ? styles.selected : ""}
+                    >
+                        {page.toUpperCase()}
+                    </button>
+                ))}
             </header>
             <section>
                 <div className={styles.wrapper1}>
                     <div className={styles.wrapper2}>
-                        <Outlet/>
+                        {selected === pages[0] && (<Grammar/>)}
+                        {selected === pages[1] && (<Dictionary/>)}
+                        {selected === pages[2] && (<Counters/>)}
+                        {selected === pages[3] && (<KanjiPage/>)}
                     </div>
                 </div>
             </section>
